8b03c949fad64319987b277e94d1ee471b7a428b
Pete X. Graham <petegraham1@gmail.com>
2011-12-23 11:09:44 +0000

    removed dry run from deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 cd /var/www/html/
 cp -R hotgirlsandmetal.com backup/files/
-rsync -n -av --delete git_hotgirlsandmetal.com/deploy/ hotgirlsandmetal.com/
+rsync -av --delete git_hotgirlsandmetal.com/deploy/ hotgirlsandmetal.com/
 cd -
