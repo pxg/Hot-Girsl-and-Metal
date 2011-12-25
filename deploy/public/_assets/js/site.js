ed3f8e7aaf324dff382155badf4792c8a608c7bf
Pete X. Graham <petegraham1@gmail.com>
2011-12-25 12:13:11 +0000

    smaller video for music

--- deploy/public/_assets/js/site.js
+++ deploy/public/_assets/js/site.js
@@ -1,42 +1,41 @@
-	// Youtube code for the 2 players ////////////////////////////////////////////////////////////////
-	var tag = document.createElement('script');
-	tag.src = "http://www.youtube.com/player_api";
-	var firstScriptTag = document.getElementsByTagName('script')[0];
-	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// Youtube code for the 2 players ////////////////////////////////////////////////////////////////
+var tag = document.createElement('script');
+tag.src = "http://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-	var player;
-	var player2;
+var player;
+var player2;
 
-	function onYouTubePlayerAPIReady() {
-		player = new YT.Player('player', {
-			height: '880',
-			width: '1280',
-			videoId: 'DUT5rEU6pqM',
-			playerVars: {
-				controls: '0'
-			},
-			events: {
-				'onReady': onPlayerReady,
-				'onStateChange': onPlayerStateChange,
-				'onError': onError
-			}
-		});
+function onYouTubePlayerAPIReady() {
+	player = new YT.Player('player', {
+		height: '880',
+		width: '1280',
+		videoId: 'DUT5rEU6pqM',
+		playerVars: {
+			controls: '0'
+		},
+		events: {
+			'onReady': onPlayerReady,
+			'onStateChange': onPlayerStateChange,
+			'onError': onError
+		}
+	});
 
-		// should  the sizes be 0 here?
-		player2 = new YT.Player('player2', {
-			height: '390',
-			width: '640',
-			videoId: 'Myj-q0OpETk',
-			playerVars: {
-				controls: '0'
-			},
-			events: {
-				'onReady': onPlayerReady2,
-				'onStateChange': onPlayerStateChange,
-				'onError': onError
-			}
-		});
-	}
+	player2 = new YT.Player('player2', {
+		height: '1', // NOTE: wont work as zero
+		width: '1',
+		videoId: 'Myj-q0OpETk',
+		playerVars: {
+			controls: '0'
+		},
+		events: {
+			'onReady': onPlayerReady2,
+			'onStateChange': onPlayerStateChange,
+			'onError': onError
+		}
+	});
+}
 
 function onPlayerReady(event) {
 	event.target.setVolume(0);
@@ -52,14 +51,9 @@ function onError(event) {
 	alert('on error');
 }
 
-// The API calls this function when the player's state changes.
-// The function indicates that when playing a video (state=1),
-// the player should play for six seconds and then stop.
-var done = false;
 function onPlayerStateChange(event) {
-	if (event.data == YT.PlayerState.PLAYING && !done) {
-		//setTimeout(stopVideo, 6000);
-		done = true;
+	if(event.data === 0) {
+		$('#next').click();
 	}
 }
 
@@ -80,43 +74,17 @@ $(document).ready(function() {
 	
 	function parseXml(xml)
 	{
-		//find every Tutorial and print the author
 		$(xml).find("item").each(function()
 		{
-			//alert($(this).find("video").attr("youtube_id"));
 			videos.push($(this).find("video").attr("youtube_id"));
 			songs.push($(this).find("song").attr("youtube_id"));
 		});
-		//alert(videos);
-		//alert(songs);
 	}
-
-/*
-// Move to next track when button is pressed //////////////////////////////////////////////////////////////////////
-var videos =new Array();
-videos[0] = 'UCK0L6vqn8U'; // jessica simpson
-videos[1] = 'TKfl94x3ptg';
-videos[2] = 'VCLxJd1d84s'; // TODO need to skip intro (put in xml)
-videos[3] = 'MakgTQ_Ubzs';
-
-var songs =new Array();
-//songs[0] = 'F_6IjeprfEs'; //roots
-songs[0] = '7RJsRQOneMY'; //replica
-songs[1] = '_mWPPBW4DU8'; // davidian
-songs[2] = '9d4ui9q7eDM'; // holly wars
-songs[3] = 'AJ0sW7KOFhU'; // warriors of the world unite
-*/
-
-// TODO move to next track at the end of song/video too
-//$(window).load(function(){
-//$(document).ready(function() {
-	// detect next button click
+	
 	$("#next").click(function() {
 		var id = (Math.random() * videos.length) | 0;
 		var nextVideo = videos[id];
 		var nextSong = songs[id];
-		//TODO: get array from xml?
-		//TODO: toggle on randomisation and coupling
 		player.loadVideoById(nextVideo);
 		player2.loadVideoById(nextSong);
 	});
