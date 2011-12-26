ec16a9661844337c7943842474c471cb60f94a05
Pete X. Graham <petegraham1@gmail.com>
2011-12-26 16:24:29 +0000

    html5 doctype dropped due to it making the videos very small

--- deploy/public/index.php
+++ deploy/public/index.php
@@ -1,5 +1,9 @@
 <html>
 	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="Watch Videos of Hot Girls and listen to Metal! \m/">
+		<meta name="author" content="Pete X. Graham">
+		
 		<title>Hot Girls and Metal</title>
 		<script type="text/javascript" src="/_assets/js/lib/jquery-1.7.1.min.js"></script>
 		<script type="text/javascript" src="/_assets/js/lib/jquery.fitvids.js"></script>
@@ -16,20 +20,20 @@
 	<body id="home">
 		<div id="player"></div>
 		<div id="player2"></div>
+	
+		<!--
+		TODO:
+		1. animate out footer (display over video), leave few pixels for reactivation
+		2. previous button (needs deep link logic first?)
+		3. toggle show/hide nav
+		4. dynamic info in footer
+		-->
+	
+		<div id="next"><a href="#">Next</a></div>
+	
+		<footer>
+			<p>Produced by <a target="_blank" href="http://www.guntlondon.com" class="gunt-logo">GUNT</a></p>
+		</footer>
 	</body>
-	
-	<!--
-	TODO:
-	1. animate out footer (display over video), leave few pixels for reactivation
-	2. previous button (needs deep link logic first?)
-	3. toggle show/hide nav
-	4. dynamic info in footer
-	-->
-	
-	<div id="next"><a href="#">Next</a></div>
-	
-	<footer>
-		<p>Produced by <a target="_blank" href="http://www.guntlondon.com" class="gunt-logo">GUNT</a></p>
-	</footer>
 	
 </html>
